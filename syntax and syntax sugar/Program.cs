af7682d667daf68551be128947b21912804e3f5c
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-11 13:00:13 -0400

    syntax sugar exercise

---
+++ syntax and syntax sugar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace syntax_and_syntax_sugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int answer = 4; // explicit typing
+            //string response; // explicit typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
